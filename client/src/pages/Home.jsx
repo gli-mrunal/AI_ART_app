59ac44fa64da909b1a51afd4f665512ab35e16a3
JonBG777 <113459821+JonBG777@users.noreply.github.com>
2023-03-03 16:59:53 -0800

    edited create post and home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect} from 'react';
 
 import { Loader, Card, FormField} from '../components';
+
+const RenderCards = ({data, title}) => {
+  if(data?.length > 0) {
+  return data.map((post) => <Card key = {post._id}{...post }/>)
+  }
+  return (
+    <h2 classname = "mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}
+
+    </h2>
+  )
+
+}
 const Home = () => {
 
   const [loading, setLoading] = useState(false);
@@ -42,7 +54,21 @@ const Home = () => {
                 Showing results for <span className="text-[#222328]">{searchText}</span>
               </h2>
             )}
+            <div classname = "grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
+              {searchText ? (
+                <RenderCards
+                data = {[]}
+                title = "No search results found"
+                />
+              ) : (
+                <RenderCards 
+                  data = {[]}
+                  title = "No posts found"
+                />
+              )}
 
+
+            </div>
           </>     
 
         )}
